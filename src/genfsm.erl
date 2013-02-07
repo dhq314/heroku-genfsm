787ec54050081f28d812e13a2167d255aa213434
dhq <dhq314@gmail.com>
2013-02-07 15:05:46 +0800

    init

---
+++ src/genfsm.erl
@@ -0,0 +1,48 @@
+%% @author author <author@example.com>
+%% @copyright YYYY author.
+
+%% @doc genfsm startup code
+
+-module(genfsm).
+-author('author <author@example.com>').
+-export([start/0, start_link/0, stop/0]).
+
+ensure_started(App) ->
+    case application:start(App) of
+        ok ->
+            ok;
+        {error, {already_started, App}} ->
+            ok
+    end.
+
+%% @spec start_link() -> {ok,Pid::pid()}
+%% @doc Starts the app for inclusion in a supervisor tree
+start_link() ->
+    ensure_started(inets),
+    ensure_started(crypto),
+    ensure_started(mochiweb),
+    application:set_env(webmachine, webmachine_logger_module, 
+                        webmachine_logger),
+    ensure_started(webmachine),
+    genfsm_sup:start_link().
+
+%% @spec start() -> ok
+%% @doc Start the genfsm server.
+start() ->
+    ensure_started(inets),
+    ensure_started(crypto),
+    ensure_started(mochiweb),
+    application:set_env(webmachine, webmachine_logger_module, 
+                        webmachine_logger),
+    ensure_started(webmachine),
+    application:start(genfsm).
+
+%% @spec stop() -> ok
+%% @doc Stop the genfsm server.
+stop() ->
+    Res = application:stop(genfsm),
+    application:stop(webmachine),
+    application:stop(mochiweb),
+    application:stop(crypto),
+    application:stop(inets),
+    Res.
