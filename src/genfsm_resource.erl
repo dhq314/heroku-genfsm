1fb626deb232791296eef1707dd2963831e88d47
dhq <dhq314@gmail.com>
2013-02-19 21:15:54 +0800

    templates change

--- src/genfsm_resource.erl
+++ src/genfsm_resource.erl
@@ -10,6 +10,32 @@
 init([]) -> {ok, undefined}.
 
 to_html(ReqData, State) ->
-    {ok, Html} = genfsm_dtl:render([{param, "genfsm"}]),
+    {ok, ApplicationName} = application:get_application(?MODULE),
+    Port = list_to_integer(os:getenv("PORT")),
+    %Port = case os:getenv("WEBMACHINE_PORT") of
+     %      false -> 8000;
+      %     AnyPort -> AnyPort
+       %   end,
+
+    SchedulerId = erlang:system_info(scheduler_id),
+    SchedulerNum = erlang:system_info(schedulers),
+    ProcessCount = erlang:system_info(process_count),
+    ProcessLimit = erlang:system_info(process_limit),
+    ProcessesMemUsed = erlang:memory(processes_used),
+    ProcessesMemAlloc = erlang:memory(processes),
+    MemTotal = erlang:memory(total),
+
+    HtmlData = [
+        {application_name, ApplicationName}, 
+        {port, Port},
+        {scheduler_id, SchedulerId},
+        {scheduler_num, SchedulerNum},
+        {process_count, ProcessCount},
+        {process_limit, ProcessLimit},
+        {processes_used, ProcessesMemUsed},
+        {processes, ProcessesMemAlloc},
+        {memtotal, MemTotal}
+    ],
+
+    {ok, Html} = genfsm_dtl:render(HtmlData),
     {Html, ReqData, State}.
-    %{"<html><body><h2 style='text-align: center;'>Hi, welcome to my heroku erlang app!<h2></body></html>", ReqData, State}.
