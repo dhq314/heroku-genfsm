089f62f4d86eab3a5211625c3bf9b41beb0748fc
dhq <dhq314@gmail.com>
2013-03-05 06:08:16 +0800

    change

--- src/mod/erlshell/erlshell_action_resource.erl
+++ src/mod/erlshell/erlshell_action_resource.erl
@@ -8,7 +8,7 @@
 ]).
 -include_lib("webmachine/include/webmachine.hrl").
 
--define(HEART_TIME_INTERVAL, 10).
+-define(HEART_TIME_INTERVAL, 20).
 
 
 init(_) -> {ok, undefined}.
@@ -19,36 +19,29 @@ allowed_methods(ReqData, Context) ->
 content_types_provided(ReqData, Context) ->
     {[{"application/json", to_json}], ReqData, Context}.
 
-%% hit this with
-%%   curl "http://localhost:8000/formjson?one=two&me=pope"
 to_json(ReqData, Context) ->
     {json_body(wrq:req_qs(ReqData)), ReqData, Context}.
 
-%% hit this with
-%%   curl -X POST http://localhost:8000/formjson \
-%%        -d "one=two&me=pope"
 process_post(ReqData, Context) ->
-    PostList = mochiweb_util:parse_qs(wrq:req_body(ReqData)),
+    PostQueryList = mochiweb_util:parse_qs(wrq:req_body(ReqData)),
     Body = 
-        case get_post_value(PostList, action) of
-            false ->
+        case proplists:get_value("action", PostQueryList) of
+            undefined ->
                 [{result, 2}];
             ActionCode ->
-                %io:format("Sta ~p~n", [ActionCode]),
                 case util:string_to_term(ActionCode) of
                     1 ->
                         erlshell_create(ActionCode);
                     2 ->
-                        erlshell_stop(PostList, ActionCode);
+                        erlshell_stop(PostQueryList, ActionCode);
                     3 ->
-                        erlshell_eval(PostList, ActionCode);
+                        erlshell_eval(PostQueryList, ActionCode);
                     4 ->
-                        erlshell_heart(PostList, ActionCode);
+                        erlshell_heart(PostQueryList, ActionCode);
                     _ ->
                         [{result, 2}]
                 end
         end,
-    %io:format("Body ~p~n", [Body]),
     {true, wrq:append_to_response_body(json_body(Body), ReqData), Context}.
 
 json_body(QS) -> mochijson:encode({struct, QS}).
@@ -66,32 +59,30 @@ erlshell_create(ActionCode) ->
 
 
 %% @doc 关闭ErlShell
-erlshell_stop(PostList, ActionCode) ->
-    case get_process_name(PostList, pid) of
+erlshell_stop(PostQueryList, ActionCode) ->
+    case get_process_name(PostQueryList) of
         undefined ->
             skip;
         Pid ->
-            %io:format("Exit Pid ~p~n", [Pid]),
             exit(Pid, kill)
     end,
     [{result, 1}, {action, ActionCode}].
 
 %% @doc 解析erlstr
-erlshell_eval(PostList, ActionCode) ->
-    case get_process_name(PostList, pid) of
+erlshell_eval(PostQueryList, ActionCode) ->
+    case get_process_name(PostQueryList) of
         undefined ->
             %% 进程异常关闭，通知前端重新启动 ErlShell
             [{result, 31}, {action, ActionCode}];
         Pid ->
-            ErlStr = get_post_value(PostList, erl_str),
-            %io:format("ErlStr ~p~n", [ErlStr]),
+            ErlStr = proplists:get_value("erl_str", PostQueryList),
             Ret = gen_server:call(Pid, {'EVAL_ErlStr', ErlStr}),
             [{action, ActionCode}] ++ Ret
     end.
 
 %% @doc ErlShell 的心跳包
-erlshell_heart(PostList, ActionCode) ->
-    case get_process_name(PostList, pid) of
+erlshell_heart(PostQueryList, ActionCode) ->
+    case get_process_name(PostQueryList) of
         undefined ->
             %% 进程关闭，通知前端关掉定时器
             [{result, 41}, {action, ActionCode}];
@@ -99,20 +90,10 @@ erlshell_heart(PostList, ActionCode) ->
             gen_server:cast(Pid, 'ERLSHELL_HEART'),
             [{result, 1}, {action, ActionCode}]
     end.
-    
-
-%% @doc 获取 POST 过来的值
-get_post_value(PostList, Key) ->
-    case lists:keyfind(util:term_to_string(Key), 1, PostList) of
-        false ->
-            false;
-        {_Key, Value} ->
-            Value
-    end.
 
 %% @doc 获取进程名字
-get_process_name(PostList, Key) ->
-    ProcessNameStr = get_post_value(PostList, Key),
+get_process_name(PostQueryList) ->
+    ProcessNameStr = proplists:get_value("pid", PostQueryList),
     ProcessName = util:string_to_term(ProcessNameStr),
     case whereis(ProcessName) of
         Pid when is_pid(Pid) ->
